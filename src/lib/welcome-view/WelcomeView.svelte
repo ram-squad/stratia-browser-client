c4d867ef810e4d85471b7fa4c578b6427b1f467a
Rafał Majewski <goodheropl@gmail.com>
2023-12-02 00:54:40 +0100

    Start a new play button

--- src/lib/welcome-view/WelcomeView.svelte
+++ src/lib/welcome-view/WelcomeView.svelte
@@ -1,3 +1,17 @@
+<script lang="ts" strictEvents>
+	import {createPlay} from "$lib/play/createPlay.ts";
+	import {savePlayToLocalStorage} from "$lib/play/savePlayToLocalStorage.ts";
+
+	export let gotoPlay: (playId: string) => Promise<void>;
+
+	const handleStartNewPlayButtonClick = async () => {
+		const newPlay = createPlay();
+		savePlayToLocalStorage(newPlay);
+		await gotoPlay(newPlay.id);
+	};
+</script>
+
 <main>
 	<h1>Stratia</h1>
+	<button on:click={handleStartNewPlayButtonClick} type="button"> Start a new play </button>
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts" strictEvents>
+	import * as AppNavigation from "$app/navigation";
 	import WelcomeView from "$lib/welcome-view/WelcomeView.svelte";
+
+	const gotoPlay = async (playId: string) => {
+		await AppNavigation.goto(`/play/${playId}`);
+	};
 </script>
 
-<WelcomeView />
+<WelcomeView {gotoPlay} />
