67728a2a2f7b64a795956497848ac60f369282ef
Rafał Majewski <goodheropl@gmail.com>
2023-12-03 20:18:40 +0100

    "savePlayToLocalStorage" util function

---
+++ src/lib/play/savePlayToLocalStorage.ts
@@ -0,0 +1,8 @@
+import type {Play} from "$lib/play/Play.ts";
+import {computePlayLocalStorageItemKey} from "$lib/play/computePlayLocalStorageItemKey.ts";
+
+export function savePlayToLocalStorage(play: Play): void {
+	const itemKey = computePlayLocalStorageItemKey(play.id);
+	const itemValue = JSON.stringify(play);
+	localStorage.setItem(itemKey, itemValue);
+}
