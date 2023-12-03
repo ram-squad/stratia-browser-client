5e430f474e88e1bc5c6f96609df2ef68658b0931
Rafał Majewski <goodheropl@gmail.com>
2023-12-03 21:48:49 +0100

    Stub play view

--- src/lib/play/PlayView.svelte
+++ src/lib/play/PlayView.svelte
@@ -1,1 +1,11 @@
-<main></main>
+<script lang="ts" strictEvents>
+	import {loadPlayFromLocalStorage} from "./loadPlayFromLocalStorage";
+
+	export let playId: string;
+
+	$: play = loadPlayFromLocalStorage(playId);
+</script>
+
+<main>
+	<h1>Play "{play.id}"</h1>
+</main>
--- src/routes/play/[playId]/+page.svelte
+++ src/routes/play/[playId]/+page.svelte
@@ -1,5 +1,12 @@
 <script lang="ts" strictEvents>
+	import * as AppEnvironment from "$app/environment";
+	import * as AppStores from "$app/stores";
 	import PlayView from "$lib/play/PlayView.svelte";
+
+	const {page} = AppStores;
+	$: ({playId} = $page.params);
 </script>
 
-<PlayView />
+{#if playId !== undefined && AppEnvironment.browser}
+	<PlayView {playId} />
+{/if}
