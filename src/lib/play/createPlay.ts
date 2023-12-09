cfb126f10593400a84c3754eb8f66179f812980a
Rafał Majewski <goodheropl@gmail.com>
2023-12-09 21:14:07 +0100

    Rename "generateNewPlayId" to "generateNewPlayID"

--- src/lib/play/createPlay.ts
+++ src/lib/play/createPlay.ts
@@ -1,8 +1,8 @@
 import type {Play} from "$lib/play/Play.ts";
-import {generateNewPlayId} from "$lib/play/generateNewPlayId.ts";
+import {generateNewPlayID} from "$lib/play/generateNewPlayID.ts";
 
 export function createPlay(): Play {
 	return {
-		id: generateNewPlayId(),
+		id: generateNewPlayID(),
 	};
 }
