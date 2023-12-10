31ca244c18a528fe4ced7268581a380b12d3e43d
Rafał Majewski <goodheropl@gmail.com>
2023-12-10 17:46:28 +0100

    "computeSideOpacity" function"

---
+++ src/lib/hex-board/tile/computeSideOpacity.ts
@@ -0,0 +1,8 @@
+import type {HexTile} from "$lib/hex/HexTile.ts";
+
+const noNeighborSideOpacity = 1;
+const neighborSideOpacity = 0.5;
+
+export function computeSideOpacity(sideNeighbor: HexTile | null): number {
+	return sideNeighbor === null ? noNeighborSideOpacity : neighborSideOpacity;
+}
