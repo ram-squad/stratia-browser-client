08f00948afe6db3aa7448c37dd24f150bd3a86f3
Rafał Majewski <goodheropl@gmail.com>
2023-12-15 21:35:23 +0100

    Ticks in "PlayView"

--- src/lib/play/PlayView.svelte
+++ src/lib/play/PlayView.svelte
@@ -1,11 +1,36 @@
 <script lang="ts" strictEvents>
 	import {HexGrid} from "$lib/hex/HexGrid.ts";
 	import HexBoard from "$lib/hex-board/HexBoard.svelte";
+	import {convertSecondsToMiliseconds} from "$lib/math/convertSecondsToMiliseconds.ts";
+	import {computePlayTick} from "$lib/play/computePlayTick.ts";
 	import {loadPlayFromLocalStorage} from "$lib/play/loadPlayFromLocalStorage.ts";
+	import {onDestroy} from "svelte";
 
 	export let playID: string;
 
-	$: play = loadPlayFromLocalStorage(playID);
+	const tickIntervalSeconds = 0.1;
+	let lastPlayID = playID;
+	let play = loadPlayFromLocalStorage(playID);
+
+	const tick = () => {
+		play = computePlayTick(play, tickIntervalSeconds);
+	};
+
+	const setupTickInterval = () =>
+		setInterval(tick, convertSecondsToMiliseconds(tickIntervalSeconds));
+
+	let tickIntervalID = setupTickInterval();
+
+	$: if (lastPlayID !== playID) {
+		play = loadPlayFromLocalStorage(playID);
+		clearInterval(tickIntervalID);
+		tickIntervalID = setupTickInterval();
+		lastPlayID = playID;
+	}
+
+	onDestroy(() => {
+		clearInterval(tickIntervalID);
+	});
 </script>
 
 <main class="play-view">
