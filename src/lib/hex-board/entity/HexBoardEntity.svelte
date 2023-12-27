fea748aff203e7736a00879bed38886a8a750c8b
Rafał Majewski <goodheropl@gmail.com>
2023-12-27 16:35:51 +0100

    Display health bars over entities

--- src/lib/hex-board/entity/HexBoardEntity.svelte
+++ src/lib/hex-board/entity/HexBoardEntity.svelte
@@ -18,11 +18,9 @@
 		layoutStylesCalculationPrecission * entity.position.y
 	).toString()}px - 50%)`;
 
-	$: entityTransformTranslateStyle = `translate(${entityTransformTranslateXStyle}, ${entityTransformTranslateYStyle})`;
+	$: entityTransformStyle = `translate(${entityTransformTranslateXStyle}, ${entityTransformTranslateYStyle})`;
 
-	$: entityTransformRotateStyle = `rotate(${entity.directionRadians.toString()}rad)`;
-
-	$: entityTransformStyle = `${entityTransformTranslateStyle} ${entityTransformRotateStyle}`;
+	$: iconTransformStyle = `rotate(${entity.directionRadians.toString()}rad)`;
 
 	const eventDispatcher = createEventDispatcher<{
 		"icon-click": Entity;
@@ -34,11 +32,17 @@
 </script>
 
 <li class="hex-board-entity" style:transform={entityTransformStyle}>
+	<progress
+		class="hex-board-entity__health-bar"
+		max={entity.maximalHealthPoints.toString()}
+		value={entity.healthPoints.toString()}
+	/>
 	<button
 		class="hex-board-entity__icon"
 		class:hex-board-entity__icon--selected={isSelected}
 		on:click={handleIconClick}
 		style:font-size={iconFontSizeStyle}
+		style:transform={iconTransformStyle}
 		type="button"
 	>
 		🧍
@@ -65,5 +69,24 @@
 	.hex-board-entity__icon--selected {
 		background-color: hsl(0deg 0% 0% / 0.1);
 		outline: 2px solid hsl(0deg 0% 0% / 1);
+	}
+
+	.hex-board-entity__health-bar {
+		$health-bar-width: 10rem !default;
+		$health-bar-height: 1rem !default;
+		border-radius: 0;
+		height: $health-bar-height;
+		left: calc($health-bar-width / -2 + 50%);
+		position: absolute;
+		top: -$health-bar-height;
+		width: $health-bar-width;
+
+		&::-webkit-progress-bar {
+			background-color: hsl(0deg 100% 50% / 1);
+		}
+
+		&::-webkit-progress-value {
+			background-color: hsl(120deg 100% 50% / 1);
+		}
 	}
 </style>
