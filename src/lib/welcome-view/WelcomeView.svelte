40cbb89a700b176fff28237997348ec707074e44
MackowiakMateusz <Darkstaron123@gmail.com>
2023-12-28 21:05:47 +0100

    Add init view

--- src/lib/welcome-view/WelcomeView.svelte
+++ src/lib/welcome-view/WelcomeView.svelte
@@ -24,47 +24,46 @@
 
 <style>
   .background {
-    background-color: black;
+    background-color: rgb(0 0 0);
     height: 100%;
     width: 100%;
   }
 
   .main-menu {
-    text-align: center;
-    background-color: rgb(0, 0, 46);
-    position: absolute;
+    background-color: rgb(0 0 46);
+    border: solid;
+    border-color: rgb(255 255 0);
+    border-radius: 40px;
+    border-width: 10px;
+    height: 80vh;
     margin-left: 10vw;
     margin-top: 10vh;
+    position: absolute;
+    text-align: center;
     width: 80vw;
-    height: 80vh;
-    border: solid;
-    border-width: 10px;
-    border-radius: 40px;
-    border-color: yellow;
   }
 
   /* menu-button */
+
   .menu-button {
-    background-color: rgb(0, 174, 255);
-    color: rgb(0, 0, 0);
-    border: 2px solid rgb(216, 2, 134);
-    border-radius: 0px;
-    padding: 18px 36px;
+    background-color: rgb(0 174 255);
+    border: 2px solid rgb(216 2 134);
+    border-radius: 0;
+    box-shadow: inset 0 0 0 0 #ff009d;
+    color: rgb(0 0 0);
+    cursor: pointer;
     display: inline-block;
-    font-family: "Lucida Console", Monaco, monospace;
+    font-family: "Lucida Console", "Monaco", monospace;
     font-size: 24px;
     font-weight: 4px;
     letter-spacing: 1px;
-    cursor: pointer;
-    box-shadow: inset 0 0 0 0 #ff009d;
-    -webkit-transition: ease-out 0.4s;
-    -moz-transition: ease-out 0.4s;
+    padding: 18px 36px;
     transition: ease-out 0.4s;
   }
 
   .menu-button:hover {
+    color: rgb(255 230 0);
     font-size: 24px;
-    color: rgb(255, 230, 0);
   }
 
   .slide_down:hover {
@@ -82,35 +81,37 @@
   .slide_diagonal:hover {
     box-shadow: inset 400px 50px 0 0 #bb5400;
   }
+
   /* end menu-button */
 
   /* game-title-text */
+
   .game-title-text {
     background: linear-gradient(to bottom, #cfc09f 22%, #cfc09f 26%, #cfc09f 27%, #ffecb3 40%, #3a2c0f 78%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    color: #fff;
+    background-clip: text;
+    color: #ffffff;
     font-family: 'Playfair Display', serif;
-    position: relative;
-    text-transform: uppercase;
     font-size: 14vw;
-    margin: 0;
     font-weight: 400;
-    animation: game 0s 5s;
+    margin: 0;
+    position: relative;
+    -webkit-text-fill-color: transparent;
+    text-transform: uppercase;
   }
 
-  .game-title-text:after {
+  .game-title-text::after {
     background: none;
     content: attr(data-heading);
     left: 0;
-    top: 0;
-    z-index: -1;
     position: absolute;
     text-shadow:
       -1px 0 1px #c6bb9f,
       0 1px 1px #c6bb9f,
-      5px 5px 10px rgba(0, 0, 0, 0.4),
-      -5px -5px 10px rgba(0, 0, 0, 0.4);
+      5px 5px 10px rgb(0 0 0 / 0.4),
+      -5px -5px 10px rgb(0 0 0 / 0.4);
+    top: 0;
+    z-index: -1;
   }
+
   /* end game-title-text */
 </style>
