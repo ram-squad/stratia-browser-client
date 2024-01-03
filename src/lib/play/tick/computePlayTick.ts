3022f4a2663f7408acecd3775da18eec840eece9
Rafał Majewski <goodheropl@gmail.com>
2024-01-04 00:44:30 +0100

    WIP 3

--- src/lib/play/tick/computePlayTick.ts
+++ src/lib/play/tick/computePlayTick.ts
@@ -1,3 +1,5 @@
+import type {Point} from "$lib/math/point/Point.ts";
+import {calculateDistanceBetweenTwoPoints} from "$lib/math/point/calculateDistanceBetweenTwoPoints.ts";
 import type {Play} from "$lib/play/Play.ts";
 import type {Entity} from "../entity/Entity.ts";
 
@@ -14,18 +16,39 @@ export function computePlayTick(play: Play, deltaTimeSeconds: number): Play {
 						) +
 						(Math.PI * 3) / 2;
 
+			const newSpeedPerSecond = entity.targetPosition === null ? 0 : 100;
+
+			const newPotentialPosition: Point = {
+				x: entity.position.x - Math.sin(newDirectionRadians) * newSpeedPerSecond * deltaTimeSeconds,
+				y: entity.position.y + Math.cos(newDirectionRadians) * newSpeedPerSecond * deltaTimeSeconds,
+			};
+
+			const {newPosition, newTargetPosition} =
+				entity.targetPosition === null
+					? {
+							newPosition: newPotentialPosition,
+							newTargetPosition: null,
+						}
+					: calculateDistanceBetweenTwoPoints(newPotentialPosition, entity.targetPosition) >
+						  calculateDistanceBetweenTwoPoints(entity.position, entity.targetPosition)
+						? {
+								newPosition: entity.targetPosition,
+								newTargetPosition: null,
+							}
+						: {
+								newPosition: newPotentialPosition,
+								newTargetPosition: entity.targetPosition,
+							};
+
 			const updatedEntity: Entity = {
 				...entity,
 				directionRadians: newDirectionRadians,
-				position: {
-					x:
-						entity.position.x -
-						Math.sin(newDirectionRadians) * entity.speedPerSecond * deltaTimeSeconds,
-					y:
-						entity.position.y +
-						Math.cos(newDirectionRadians) * entity.speedPerSecond * deltaTimeSeconds,
-				},
+				position: newPosition,
+				speedPerSecond: newSpeedPerSecond,
+				targetPosition: newTargetPosition,
 			};
+
+			console.log(updatedEntity);
 
 			return updatedEntity;
 		}),
