57f97ee84243d2b85abfec3bc97c8c60d1bec00d
Rafał Majewski <goodheropl@gmail.com>
2024-01-03 22:35:34 +0100

    Changing entity selection mode

--- src/lib/play/view/selected-entity-bar/SelectedEntityBar.svelte
+++ src/lib/play/view/selected-entity-bar/SelectedEntityBar.svelte
@@ -2,8 +2,22 @@
 
 <script lang="ts" strictEvents>
 	import type {EntitySelection} from "$lib/play/entity/selection/EntitySelection.ts";
+	import type {EntitySelectionMode} from "$lib/play/entity/selection/mode/EntitySelectionMode.ts";
+	import {createEventDispatcher} from "svelte";
 
 	export let entitySelection: EntitySelection | null;
+
+	const eventDispatcher = createEventDispatcher<{
+		"mode-change-requested": EntitySelectionMode | null;
+	}>();
+
+	const handleModeGoButtonClicked = () => {
+		eventDispatcher("mode-change-requested", "go");
+	};
+
+	const handleCancelModeButtonClicked = () => {
+		eventDispatcher("mode-change-requested", null);
+	};
 </script>
 
 <div>
@@ -11,5 +25,10 @@
 		No entity selected.
 	{:else}
 		Selected entity: {entitySelection.entity.id}
+		{#if entitySelection.mode === null}
+			<button on:click={handleModeGoButtonClicked} type="button">Go</button>
+		{:else}
+			<button on:click={handleCancelModeButtonClicked} type="button">Cancel</button>
+		{/if}
 	{/if}
 </div>
